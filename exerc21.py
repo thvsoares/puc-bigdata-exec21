cb5a7b6e9b16e2bdc3e02f4e312eb0e4c59482ad
Thiago Vinicius Soares <1055485@sga.pucminas.br>
2018-04-17 19:43:10 -0300

    mapfn

--- exerc21.py
+++ exerc21.py
@@ -4,7 +4,7 @@ import csv
 
 text_files = glob.glob('\\temp\\arq_exec\\textos')
 
-def file_contents(file_name)
+def file_contents(file_name):
     f = open(file_name)
     try:
         return f.read()
@@ -12,3 +12,11 @@ def file_contents(file_name)
         f.close()
     
 source = dict((file_name, file_contents(file_name))for file_name in text_files)
+
+def mapfn(k,v):
+    print 'map ' + k
+    from stopwords import allStopWords
+    for line in v.splitlines():
+        for word in line.split():
+            if (word not in allStopWords):
+                yield word, 1
